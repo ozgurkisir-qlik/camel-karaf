a3863fb61d1f4622e14bbc41227688a16e3b2146
JB Onofré <jbonofre@apache.org>
2024-03-11 08:00:53 +0100

    Update

--- components/camel-directvm/src/main/java/org/apache/camel/karaf/component/directvm/DirectVmComponent.java
+++ components/camel-directvm/src/main/java/org/apache/camel/karaf/component/directvm/DirectVmComponent.java
@@ -17,10 +17,13 @@
 package org.apache.camel.karaf.component.directvm;
 
 import org.apache.camel.Endpoint;
+import org.apache.camel.spi.HeaderFilterStrategy;
 import org.apache.camel.spi.Metadata;
 import org.apache.camel.spi.annotations.Component;
 import org.apache.camel.support.DefaultComponent;
 
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicInteger;
@@ -36,12 +39,67 @@ public class DirectVmComponent extends DefaultComponent {
     private static final ConcurrentMap<String, DirectVmConsumer> CONSUMERS = new ConcurrentHashMap<>();
     @Metadata(label = "producer", defaultValue = "true")
     private boolean block = true;
+    @Metadata(label = "producer", defaultValue = "30000")
+    private long timeout = 30000L;
+    @Metadata(label = "advanced")
+    private HeaderFilterStrategy headerFilterStrategy;
+    @Metadata(label = "advanced", defaultValue = "true")
+    private boolean propagateProperties = true;
 
     public DirectVmComponent() {}
 
+    public static Collection<Endpoint> getConsumerEndpoints() {
+        Collection<Endpoint> endpoints = new ArrayList<>(CONSUMERS.size());
+        for (DirectVmConsumer consumer : CONSUMERS.values()) {
+            endpoints.add(consumer.getEndpoint());
+        }
+        return endpoints;
+    }
+
     @Override
     protected Endpoint createEndpoint(String uri, String remaining, Map<String, Object> parameters) throws Exception {
+        DirectVmEndpoint answer = new DirectVmEndpoint(uri, this);
+        answer.setBlock(block);
+        answer.setTimeout(timeout);
+        answer.setPropagateProperties(propagateProperties);
+        setProperties(answer, parameters);
+        return answer;
+    }
 
+    public DirectVmConsumer getConsumer(DirectVmEndpoint endpoint) {
+        String key = getConsumerKey(endpoint.getEndpointUri());
+        return CONSUMERS.get(key);
+    }
+
+    public void addConsumer(DirectVmEndpoint endpoint, DirectVmConsumer consumer) {
+        String key = getConsumerKey(endpoint.getEndpointUri());
+        DirectVmConsumer existing = CONSUMERS.putIfAbsent(key, consumer);
+        if (existing != null) {
+            String contextId = existing.getEndpoint().getCamelContext().getName();
+            throw new IllegalStateException("A consumer " + existing + " already exists from CamelContext: " + contextId + ". Multiple consumers not supported");
+        }
+    }
+
+    public void removeConsumer(DirectVmEndpoint endpoint) {
+        String key = getConsumerKey(endpoint.getEndpointUri());
+        CONSUMERS.remove(key);
+    }
+
+    private static String getConsumerKey(String uri) {
+        if (uri.contains("?")) {
+            // strip parameters
+            uri = uri.substring(0, uri.indexOf('?'));
+        }
+        return uri;
+    }
+
+    @Override
+    protected void doStart() throws Exception {
+        if (START_COUNTER.decrementAndGet() <= 0) {
+            // clear queues when no more direct-vm components in use
+            CONSUMERS.clear();
+        }
+        super.doStop();
     }
 
 }
